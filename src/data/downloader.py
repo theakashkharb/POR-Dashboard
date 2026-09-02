bc8cf4af3448ed353e2644c31de9b32c5cd7d704
Akash <your_email@example.com>
2026-09-02 05:38:43 +0530

    Complete 500 stock data pipeline

--- src/data/downloader.py
+++ src/data/downloader.py
@@ -2,28 +2,35 @@ import yfinance as yf
 import pandas as pd
 
 
-def download_stock(ticker, period='5y'):
-    data = yf.download(
-        ticker,
-        period=period,
-        auto_adjust=False,
-        multi_level_index=False,
-        progress=False
-    )
+def download_stock(ticker, period="20y"):
 
-    if data.empty:
+    try:
+        data = yf.download(
+            ticker,
+            period=period,
+            auto_adjust=False,
+            multi_level_index=False,
+            progress=False
+        )
+
+        if data.empty:
+            print(f"NO DATA: {ticker}")
+            return pd.DataFrame()
+
+        data = data.reset_index()
+
+        data = data[
+            ["Date", "Open", "High", "Low", "Close", "Volume"]
+        ]
+
+        data["Ticker"] = ticker
+
+        data = data[
+            ["Ticker", "Date", "Open", "High", "Low", "Close", "Volume"]
+        ]
+
+        return data
+
+    except Exception as e:
+        print(f"FAILED: {ticker} | {e}")
         return pd.DataFrame()
-
-    data = data.reset_index()
-
-    data = data[
-        ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
-    ]
-
-    data['Ticker'] = ticker
-
-    data = data[
-        ['Ticker', 'Date', 'Open', 'High', 'Low', 'Close', 'Volume']
-    ]
-
-    return data
